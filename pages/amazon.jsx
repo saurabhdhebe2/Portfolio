2c2472f8f8aedeeeadace216292dec88ce98114c
saurabhdhebe2 <108879055+saurabhdhebe2@users.noreply.github.com>
2022-10-26 18:33:52 +0530

    hdhd

--- pages/amazon.jsx
+++ pages/amazon.jsx
@@ -42,7 +42,7 @@ const netflix = () => {
             <button className="px-8 py-2 mt-4 mr-8">Code</button>
           </a>
           <a
-            href="https://fireclint.github.io/netflix-react-tailwind/"
+            href="https://amazon-clone-coral-alpha.vercel.app/"
             target="_blank"
             rel="noreferrer"
           >

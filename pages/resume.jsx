a12006b1268c18d6033ffd9039aa0ef1f0b68592
saurabhdhebe2 <108879055+saurabhdhebe2@users.noreply.github.com>
2022-10-26 18:00:01 +0530

    kjb

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -19,7 +19,7 @@ const resume = () => {
           <h2 className="text-center">Saurabh Dhebe</h2>
           <div className="flex">
             <a
-              href="https://drive.google.com/file/d/1ArFUQX_fmbWWlJg2EI63dN1xfiiognD-/view"
+              href="https://drive.google.com/file/d/1B2J8i301PIM0toKSEaisjHkX22ysOSRR/view?usp=drivesdk"
               target="_blank"
               rel="noreferrer"
             >
